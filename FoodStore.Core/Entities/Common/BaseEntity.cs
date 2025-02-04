5fcc00d6bafa7c44821392254873f6120a0baf1d
hikmataliyev059 <hikmtaliyev444@gmail.com>
2025-02-04 04:00:42 +0400

    add common properties for entities

---
+++ FoodStore.Core/Entities/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodStore.Core.Entities.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public bool IsActive { get; set; } = true;
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedTime { get; set; }
+}
